81d2b1e5d1e75311d77d98cd5cf28a628f2195cd
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-20 08:58:17 +0000

    autocommit

--- lectures/lecture02_01/main.cpp
+++ lectures/lecture02_01/main.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main()
 {
     int a,b;
+    cout << "--0--" << endl;
 
     cin >> a >> b;
-
+    cout << "--1--" << endl;
     cout << a / b << endl;
+    cout << "--2--" << endl;
     return 0;
 }
