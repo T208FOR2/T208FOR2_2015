3a746dd1d3a2c9b8a0deb25d432c44a1d496d40f
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-20 09:28:32 +0000

    autocommit

--- lectures/lecture02_01/main.cpp
+++ lectures/lecture02_01/main.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main()
 {
-    int a;
+    int a = INT_MAX;
 
+    cout << a << endl << endl;
+    cout << a - 2 << endl;
+    cout << a - 1 << endl;
     cout << a << endl;
 
     return 0;
