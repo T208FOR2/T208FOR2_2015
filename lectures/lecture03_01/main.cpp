6dfe09aefab80a09a90b9aa204e57ce6bf6d0223
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-24 13:40:15 +0000

    autocommit

---
+++ lectures/lecture03_01/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char a,b;
+    cin >> a >> b;
+    a = a + 1;
+    cout << "\"" << a << "\" - \"" << b+1 << "\"" << endl;
+
+    return 0;
+}
