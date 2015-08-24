912358bc836777b5e4922a80c5f5cd8b56f695cf
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-24 14:20:39 +0000

    autocommit

--- lectures/lecture03_01/main.cpp
+++ lectures/lecture03_01/main.cpp
@@ -6,16 +6,24 @@ int main()
     int a, b;
     cin >> a >> b;
 
-    bool success = true;
+    if (a = b) {
+        cout << a << endl;
+    }
+//    bool success = true;
 
-    success = (a % b == 0);
+//    success = (a % b == 0);
 
+/*
     if (((a % b == 0) && (a % 2 == 0) && (b >= 0)) || (a < b)) {
         cout << "yes!" << endl;
+    }
+    else if (a == b) {
+        cout << "equal!" << endl;
     }
     else {
         cout << "no" << endl;
     }
+*/
 
     return 0;
 }
