4dce8dea5c17ed3c41be8577f16a55b648ee7ed1
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-24 14:28:47 +0000

    autocommit

--- lectures/lecture03_01/main.cpp
+++ lectures/lecture03_01/main.cpp
@@ -6,14 +6,14 @@ int main()
     int a;
     cin >> a;
 
+    if (a < 100) {
+        cout << "less than 100" << endl;
+    }
     if (a < 1000) {
         cout << "less than 1000" << endl;
     }
-    else if (a < 100) {
-        cout << "less than 100" << endl;
-    }
     else {
-        cout << "very small" << endl;
+        cout << "very large" << endl;
     }
 
     return 0;
