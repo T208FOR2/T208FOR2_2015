d7dc38a17344e1f320fb0556e2d0a1e97311c4b5
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-24 13:54:26 +0000

    autocommit

--- lectures/lecture03_01/main.cpp
+++ lectures/lecture03_01/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
 
 int main()
@@ -7,8 +8,7 @@ int main()
     int a, b;
     cin >> a >> b;
 
-    cout << a << " / " << b << " = " << a / b << endl;
-    cout << a << " % " << b << " = " << a % b << endl;
+    cout << a << "^" << b << " = " << pow(a,b) << endl;
 
     return 0;
 }
