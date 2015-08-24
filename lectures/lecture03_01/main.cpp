3a3d42b158c0ee0e03b261d29c13558472c1874a
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-24 13:52:24 +0000

    autocommit

--- lectures/lecture03_01/main.cpp
+++ lectures/lecture03_01/main.cpp
@@ -7,7 +7,8 @@ int main()
     int a, b;
     cin >> a >> b;
 
-    cout << a << " / " << b << " = " << a / b;
+    cout << a << " / " << b << " = " << a / b << endl;
+    cout << a << " % " << b << " = " << a % b << endl;
 
     return 0;
 }
