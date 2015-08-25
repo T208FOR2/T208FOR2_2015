0607e6ea65850ee2fa5eefd3ef1d6e6d5e79ce2b
unknown <hjorleifur12@V102-7334.hir.is>
2015-08-25 13:36:21 +0000

    Verkefni1-2

---
+++ problem_solving_class/DT01/DT01-2/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Veldu fjolda itranir" << endl;
+    cin >> n;
+
+    double summa1 = 0.0;
+    double summa2 = 0.0;
+    int i = 1;
+
+    double j = 0.5;
+
+
+    while(i < n)
+    {
+        summa1 += (1.0/(i*i));
+        summa2 +=  j;
+        j = 0.5 * j;
+        i += 1;
+    }
+
+    cout << summa1 << endl;
+    cout << summa2 << endl;
+
+
+
+}
