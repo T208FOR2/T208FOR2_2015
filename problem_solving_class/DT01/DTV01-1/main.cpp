da724d0922a7b919ee18a11c3cc487d880f8abbd
unknown <hjorleifur12@V102-7334.hir.is>
2015-08-25 13:04:38 +0000

    Verkefni1.1 DT01

---
+++ problem_solving_class/DT01/DTV01-1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cout << "Please write two numbers: " << endl;
+
+    cin >> a;
+    cin >> b;
+
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " / " << b << " = " << a / b << endl;
+    cout << a << " % " << b << " = " << a % b << endl;
+
+
+
+
+}
