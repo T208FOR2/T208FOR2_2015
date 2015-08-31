3359b7a3a907fb6297bbb06d6246ac65558d3ac0
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-31 13:48:30 +0000

    autocommit

--- lectures/lecture05_01/main.cpp
+++ lectures/lecture05_01/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+// Function decleration
+void printtimeunits(int x, string singular, string plural);
 
 int main()
 {
     int numberofseconds;
-
-    int numberofyears;
-    int numberofdays;
-    int numberofhours;
-    int numberofminutes;
 
     const int SECONDSINYEAR = 60*60*24*365;
     const int SECONDSINDAY = 60*60*24;
@@ -16,55 +15,19 @@ int main()
     const int SECONDSINMINUTE = 60;
 
     cin >> numberofseconds;
-    if (numberofseconds > SECONDSINYEAR)
-    {
-        numberofyears = numberofseconds / SECONDSINYEAR;
-        numberofseconds = numberofseconds - numberofyears*SECONDSINYEAR;
-        cout << numberofyears << " year";
-        if (numberofyears > 1) {
-            cout << "s";
-        }
-        cout << endl;
-    }
 
-    if (numberofseconds > SECONDSINDAY)
-    {
-        numberofdays = numberofseconds / SECONDSINDAY;
-        numberofseconds = numberofseconds - numberofdays*SECONDSINDAY;
-        cout << numberofdays << " day";
-        if (numberofdays > 1) {
-            cout << "s";
-        }
-        cout << endl;
-    }
-    if (numberofseconds > SECONDSINHOUR)
-    {
-        numberofhours = numberofseconds / SECONDSINHOUR;
-        numberofseconds = numberofseconds - numberofhours*SECONDSINHOUR;
-        cout << numberofhours << " hour";
-        if (numberofhours > 1) {
-            cout << "s";
-        }
-        cout << endl;
-    }
-
-    if (numberofseconds > SECONDSINMINUTE)
-    {
-        numberofminutes = numberofseconds / SECONDSINMINUTE;
-        numberofseconds = numberofseconds - numberofminutes*SECONDSINMINUTE;
-        cout << numberofminutes << " minute";
-        if (numberofminutes > 1) {
-            cout << "s";
-        }
-        cout << endl;
-    }
-    if (numberofseconds > 0) {
-        cout << numberofseconds << " second";
-        if (numberofseconds > 1) {
-            cout << "s";
-        }
-    }
-    cout << endl;
 
     return 0;
 }
+
+// Function definition
+void printtimeunits(int x, string singular, string plural) {
+    if (x == 1) {
+        cout << x << " " << singular << endl;
+    }
+    else if (x > 1) {
+        cout << x << " " << plural << endl;
+    }
+    return;
+}
+
