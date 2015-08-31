f9e8ff5ef47a51d2a69d5a991b042d184b6972fc
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-31 14:35:04 +0000

    autocommit

--- lectures/lecture05_01/main.cpp
+++ lectures/lecture05_01/main.cpp
@@ -8,14 +8,25 @@ int somefunction(int& tala1, int tala2, int& tala3);
 
 int main()
 {
-    int a,b,c;
+    int a,b,c,d;
     cin >> a >> b >> c;
+    cout << a << " " << b << " " << c << " " << d << endl;
 
-    cout << a << " " << b << " " << c << endl;
+    d = somefunction(a,b,c);
+
+    cout << a << " " << b << " " << c << " " << d << endl;
 
     return 0;
 }
 
 int somefunction(int& tala1, int tala2, int& tala3) {
+    cout << "FUNCTION: " << tala1 << " " << tala2 << " " << tala3 << endl;
 
+    tala1 = 42;
+    tala2 = 23;
+    tala3 = 5;
+
+    cout << "FUNCTION: " << tala1 << " " << tala2 << " " << tala3 << endl;
+
+    return 78;
 }
