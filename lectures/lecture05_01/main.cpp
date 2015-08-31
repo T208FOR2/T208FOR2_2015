47816000dca09fe7696f6306af4d128c1f7b3f27
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-31 13:28:16 +0000

    autocommit

--- lectures/lecture05_01/main.cpp
+++ lectures/lecture05_01/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
-
 int main()
 {
+    int a,b;
+    cin >> a >> b;
+
+    cout << a << " / " << b << " = " << a / b << endl;
+//    cout << "a^b = " << pow(a,b) << endl;
 
     return 0;
 }
