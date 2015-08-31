1f431028f219d0696db1496c7a3fd5ae5362f2d5
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-08-31 13:40:28 +0000

    autocommit

--- lectures/lecture05_01/main.cpp
+++ lectures/lecture05_01/main.cpp
@@ -1,21 +1,70 @@
 #include <iostream>
-#include <string>
-#include <cmath>
-
 using namespace std;
 
 int main()
 {
-    int a,b;
-    double c;
-    cin >> a >> b;
+    int numberofseconds;
 
-    c = static_cast<double>(a / b);
+    int numberofyears;
+    int numberofdays;
+    int numberofhours;
+    int numberofminutes;
 
-    cout << a << " / " << b << " = " << c << endl;
-//    cout << "a^b = " << pow(a,b) << endl;
+    const int SECONDSINYEAR = 60*60*24*365;
+    const int SECONDSINDAY = 60*60*24;
+    const int SECONDSINHOUR = 60*60;
+    const int SECONDSINMINUTE = 60;
+
+    cin >> numberofseconds;
+    if (numberofseconds > SECONDSINYEAR)
+    {
+        numberofyears = numberofseconds / SECONDSINYEAR;
+        numberofseconds = numberofseconds - numberofyears*SECONDSINYEAR;
+        cout << numberofyears << " year";
+        if (numberofyears > 1) {
+            cout << "s";
+        }
+        cout << endl;
+    }
+
+    if (numberofseconds > SECONDSINDAY)
+    {
+        numberofdays = numberofseconds / SECONDSINDAY;
+        numberofseconds = numberofseconds - numberofdays*SECONDSINDAY;
+        cout << numberofdays << " day";
+        if (numberofdays > 1) {
+            cout << "s";
+        }
+        cout << endl;
+    }
+    if (numberofseconds > SECONDSINHOUR)
+    {
+        numberofhours = numberofseconds / SECONDSINHOUR;
+        numberofseconds = numberofseconds - numberofhours*SECONDSINHOUR;
+        cout << numberofhours << " hour";
+        if (numberofhours > 1) {
+            cout << "s";
+        }
+        cout << endl;
+    }
+
+    if (numberofseconds > SECONDSINMINUTE)
+    {
+        numberofminutes = numberofseconds / SECONDSINMINUTE;
+        numberofseconds = numberofseconds - numberofminutes*SECONDSINMINUTE;
+        cout << numberofminutes << " minute";
+        if (numberofminutes > 1) {
+            cout << "s";
+        }
+        cout << endl;
+    }
+    if (numberofseconds > 0) {
+        cout << numberofseconds << " second";
+        if (numberofseconds > 1) {
+            cout << "s";
+        }
+    }
+    cout << endl;
 
     return 0;
 }
-
-
