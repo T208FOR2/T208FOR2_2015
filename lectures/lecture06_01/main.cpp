0f4e5c7584b0f9bf02e6ce561b30d52efef03c86
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-03 09:43:58 +0000

    autocommit

--- lectures/lecture06_01/main.cpp
+++ lectures/lecture06_01/main.cpp
@@ -14,18 +14,15 @@ int main()
     cout << "Please write the name of the file: ";
     cin >> thefilename;
 
-    theinputfile.open("xrandomtolur.txt");  // Mac
-//    theinputfile.open("c:\\Users\\eyjo\\Desktop\\randomtolur.txt"); // Windows
+    theinputfile.open(thefilename.c_str());
+
     if (theinputfile.fail()) {
         cout << "Can't open the file!" << endl;
         exit(1);
     }
 
-    for (i = 0; i < NR; i++) {
-        theinputfile >> tala;
-    }
 
-    cout << "The " << i << "th number is: " << tala << endl;
+
 
     theinputfile.close();
     return 0;
