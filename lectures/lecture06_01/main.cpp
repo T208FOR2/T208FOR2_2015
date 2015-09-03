e4f0a3b5f67b0624c32a3b1db2059091b4a038a9
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-03 08:51:29 +0000

    autocommit

--- lectures/lecture06_01/main.cpp
+++ lectures/lecture06_01/main.cpp
@@ -5,7 +5,15 @@ using namespace std;
 
 int main()
 {
+    ifstream theinputfile;
+    int tala;
 
+    theinputfile.open("randomtolur.txt");
 
+    cin >> tala;
+
+    cout << "The first number is: " << tala << endl;
+
+    theinputfile.close();
     return 0;
 }
