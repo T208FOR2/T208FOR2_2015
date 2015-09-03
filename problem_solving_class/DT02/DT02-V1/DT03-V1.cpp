1801fe8a956f5b40a2206931c3c8fffc4fc69e53
sarakristjans <sarakri13@ru.is>
2015-09-03 20:38:59 +0000

    Update DT03-V1.cpp

--- problem_solving_class/DT02/DT02-V1/DT03-V1.cpp
+++ problem_solving_class/DT02/DT02-V1/DT03-V1.cpp
@@ -21,8 +21,14 @@ bool isEqual(int number1, int number2);
 // og afgangsvirkjann.
 int reverseNumber(int numberToReverse);
 
+// Fall sem gerir það sama og fallið fyrir ofan, nema aðeins önnur aðferð notuð
+int reverseNumber2(int numberToReverse);
+
 // Fall sem sér um að prenta niðurstöðuna á fallegan hátt.
 void printResult(int numberToPrint, bool isPalindrome);
+
+// Onnur utfaersla a printResault fallinu. Her er hinsvegar kallad a reverseNumber og isEqual follin
+void printResult2(int number);
 
 
 int main()
@@ -71,9 +77,53 @@ int reverseNumber(int numberToReverse)
     return newNumber;
 }
 
+// Hin utfaerslan er betri thar sem einungis er ein while lykkja og forritid lengur ad keyra thetta fall.
+int reverseNumber2(int numberToReverse)
+{
+    int remainder = 0;
+    int newNumber = 0;
+    int tugur = 1;
+    
+    // Finnum fyrst hversu mörg "tugasæti" talan hefur
+    while(number > tugur * 10)
+    {
+        tugur = tugur * 10;
+    }
+    
+    while (numberToReverse != 0)
+    {
+        // Finnum afgang ur heiltoludeilingu sem verdur tha fyrsta talan i nyju tolunni
+        remainder = numberToReverse % 10;
+        // Margfoldum afganginn með þeim "tug" sem á vid, t.d 1000 eða 100
+        newNumber = newNumber + remainder*tugur;
+        // Setjum toluna sem nidurstodu deilingarinnar, verdur 0 i lokin thvi vid erum ad vinna med heiltoludeilingu
+        numberToReverse /= 10;
+        // Laekkum tugasaetið um eitt, 1000 verdur ad 100 o.s.frv
+        tugur = tugur / 10;
+    }
+    
+    return newNumber;
+}
+
 void printResult(int numberToPrint, bool isPalindrome)
 {
     if (isPalindrome)
+    {
+        cout << "The number " << numberToPrint << " is a palindrome!" << endl;
+    }
+    else
+    {
+        cout << "The number " << numberToPrint << " is not a palindrome!" << endl;
+    }
+}
+
+void printResult2(int number)
+{
+    // snuum tolunni vid
+    int reversedNumber = reverseNumber(number);
+    
+    // athugum hvort tolurnar eru eins og prentum ut videigandi skilabod
+    if (isEqual(number, reversedNumber))
     {
         cout << "The number " << numberToPrint << " is a palindrome!" << endl;
     }
