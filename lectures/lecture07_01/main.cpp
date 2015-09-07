180cfcda3bfbe35d2170573b1648ad775adea94b
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-07 14:28:53 +0000

    autocommit

--- lectures/lecture07_01/main.cpp
+++ lectures/lecture07_01/main.cpp
@@ -7,6 +7,23 @@ using namespace std;
 
 int main()
 {
+    int tala;
+    string texti;
+    char takn;
+
+    cout << "Please write something:" << endl;
+
+    if () {
+        cin >> tala;
+        cout << "You wrote a number: " << tala << endl;
+    }
+    else if () {
+        cin >> texti;
+        cout << "You wrote some text: " << texti << endl;
+    }
+    else {
+        cout << "I have no idea what this is: " << endl;
+    }
 
     return 0;
 }
