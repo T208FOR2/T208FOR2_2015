ef7e9cf3f929a9fa3be83ed2cfa038f17b41c2a0
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-07 14:26:51 +0000

    autocommit

--- lectures/lecture07_01/main.cpp
+++ lectures/lecture07_01/main.cpp
@@ -7,25 +7,6 @@ using namespace std;
 
 int main()
 {
-    string str;
-    char c;
-    ifstream infile;
-    infile.open("test.txt");
-    if (infile.fail()) {
-        cout << "Failure!" << endl;
-        exit(1);
-    }
 
-    while ( !infile.eof() ) {
-        c = infile.get();
-        cout << ">" << c << "<" << endl;
-//        cout << "\"" << c << "\"" << endl;
-    }
-
-//    while (infile >> str) {
-//        cout << "\"" << str << "\"" << endl;
-//    }
-
-    infile.close();
     return 0;
 }
