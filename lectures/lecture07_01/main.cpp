d5ed3099a10e59dbca4e4649ad549560f96b0ce2
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-07 13:26:21 +0000

    autocommit

--- lectures/lecture07_01/main.cpp
+++ lectures/lecture07_01/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
+void theoutput(ostream& outs, string thetext);
+
 int main()
 {
+    string t = "This is something\nthat I'm writing";
+    ofstream theoutputfile;
+
+    theoutput(cout, t);
 
     return 0;
+}
+
+void theoutput(ostream& outs, string thetext) {
+    outs << thetext << endl;
 }
