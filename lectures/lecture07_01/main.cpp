dc5c54d10362e5d141ca876aecfc90c59411f02b
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-07 14:02:35 +0000

    autocommit

--- lectures/lecture07_01/main.cpp
+++ lectures/lecture07_01/main.cpp
@@ -5,32 +5,8 @@
 
 using namespace std;
 
-void theoutput(ostream& outs, string thetext);
-
-void openwritefile(ofstream& thefile, string thefilename);
-
 int main()
 {
-    string t = "This is something\nthat I'm writing";
-    ofstream theoutputfile;
-    openwritefile(theoutputfile, "test2.txt");
 
-    theoutput(theoutputfile, t);
-    theoutput(cout, t);
-
-    theoutputfile.close();
     return 0;
 }
-
-void theoutput(ostream& outs, string thetext) {
-    outs << thetext << endl;
-}
-
-void openwritefile(ofstream& thefile, string thefilename) {
-    thefile.open(thefilename.c_str());
-    if (thefile.fail()) {
-        cout << "can't open the file: " << thefilename << endl;
-        exit(1);
-    }
-}
-
