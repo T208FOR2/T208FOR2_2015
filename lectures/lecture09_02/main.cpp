d7cdf3d9535a92beeb4bfb4053d9b1a09564db2f
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-14 14:16:40 +0000

    autocommit

--- lectures/lecture09_02/main.cpp
+++ lectures/lecture09_02/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+//Lesa 5 heiltolur af lyklabordi og prenta ’ ofugri rod
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int tolur[5];
+    cout << "Please write 5 numbers: ";
+
+    cin >> tolur;
+
     return 0;
 }
