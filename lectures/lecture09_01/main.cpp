058a9b88e5c53febf349696b698bc642e79516f7
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-14 13:25:54 +0000

    autocommit

--- lectures/lecture09_01/main.cpp
+++ lectures/lecture09_01/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    int a[5];
+    string texti[3];
+
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+    a[3] = 4;
+    a[4] = 5;
+
+    texti[0] = "this";
+    texti[1] = "is";
+    texti[2] = "something";
 
 
     return 0;
