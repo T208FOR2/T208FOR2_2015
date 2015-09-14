8697bed5a0292b59a058e4186310b6abd2b7a9ad
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-14 13:32:03 +0000

    autocommit

--- lectures/lecture09_01/main.cpp
+++ lectures/lecture09_01/main.cpp
@@ -3,18 +3,26 @@
 
 using namespace std;
 
+const int NUM = 10;
+
 int main()
 {
-    int a[5];
+    int a[NUM];
     string texti[3];
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < NUM; i++) {
         a[i] = i+1;
     }
 
     texti[0] = "this";
     texti[1] = "is";
     texti[2] = "something";
+
+    for (int i = 0; i < NUM; i++) {
+        cout << "Element " << i << " = " << a[i] << endl;
+    }
+
+    cout << texti[0] << " - " << texti[1] << " - " << texti[2] << endl;
 
     return 0;
 }
