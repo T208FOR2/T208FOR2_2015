6e2aa0d29525eceb394628ae88b26f03d18f93a4
Eyjólfur Ingi Ásgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-14 13:27:56 +0000

    autocommit

--- lectures/lecture09_01/main.cpp
+++ lectures/lecture09_01/main.cpp
@@ -8,16 +8,13 @@ int main()
     int a[5];
     string texti[3];
 
-    a[0] = 1;
-    a[1] = 2;
-    a[2] = 3;
-    a[3] = 4;
-    a[4] = 5;
+    for (int i = 0; i < 5; i++) {}
+        a[i] = i+1;
+    }
 
     texti[0] = "this";
     texti[1] = "is";
     texti[2] = "something";
-
 
     return 0;
 }
