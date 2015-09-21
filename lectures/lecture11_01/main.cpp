554401bcd4e6d7016577bd852edd8a9cf5d0f691
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-21 13:36:23 +0000

    autocommit

--- lectures/lecture11_01/main.cpp
+++ lectures/lecture11_01/main.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    char takn[100] = "Hi there!";
-
-    takn[9] = 'X';
-    cout << endl << "012345678901234567890" << endl;
-    cout << takn << endl << endl;
-
-    strcpy(takn,"Goodbye!");
-    cout << takn << endl << endl;
-
 
     return 0;
 }
