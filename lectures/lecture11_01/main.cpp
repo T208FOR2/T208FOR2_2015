717d4a209f777ffb6eb95e6ce2901e553629bc14
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-21 14:14:49 +0000

    autocommit

--- lectures/lecture11_01/main.cpp
+++ lectures/lecture11_01/main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 int main()
 {
+    string fnafn, enafn;
+    int ar;
+
+    cin >> fnafn >> enafn >> ar;
 
 
+
+    cout << fnafn[0] << enafn << ar << "@gmail.com" << endl;
 
     return 0;
 }
