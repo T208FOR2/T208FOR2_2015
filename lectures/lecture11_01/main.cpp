21804906c3c086050726de5961eaffb8968a25d7
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-21 11:59:17 +0000

    autocommit

--- lectures/lecture11_01/main.cpp
+++ lectures/lecture11_01/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+
     return 0;
 }
--- lectures/lecture11_02/main.cpp
+++ lectures/lecture11_02/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    string t = "This is a text to see how often the word \"the\" occurs.  The simple function does not deal with capital letters, the word we're looking for has to be exacly the same as the word in the text.";
 
-
-
+    cout << t << endl;
     return 0;
 }
