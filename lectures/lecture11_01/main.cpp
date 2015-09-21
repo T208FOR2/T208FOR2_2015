22b1366e178b3b64328562db414f7dbe0b6f9ce2
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-21 13:28:19 +0000

    autocommit

--- lectures/lecture11_01/main.cpp
+++ lectures/lecture11_01/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
+    char takn[100] = "Hi there!";
+
+    takn[9] = 'X';
+    cout << endl << "012345678901234567890" << endl;
+    cout << takn << endl << endl;
 
 
     return 0;
