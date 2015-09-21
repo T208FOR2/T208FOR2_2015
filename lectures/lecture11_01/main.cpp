027b0a7bad726d4f59777d8c6347bd2721ab0757
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-21 13:38:26 +0000

    autocommit

--- lectures/lecture11_01/main.cpp
+++ lectures/lecture11_01/main.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main()
 {
+    string a,b;
+
+    cin >> a >> b;
+
+    if (a <= b) {
+        cout << a << " is before " << b << endl;
+    }
+    else {
+        cout << a << " is after " << b << endl;
+    }
 
     return 0;
 }
