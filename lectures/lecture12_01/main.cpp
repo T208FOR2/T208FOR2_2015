5b1017354a46bcce28098f088edc3a0241516d2a
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-24 08:37:40 +0000

    autocommit

--- lectures/lecture12_01/main.cpp
+++ lectures/lecture12_01/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
 
 using namespace std;
 
+const string THEFILENAME = "twitterfeed.txt";
+
 int main()
 {
+
 
     return 0;
 }
