f62d790cf2985a16d29e64c5253f2bacdd47852f
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-24 09:36:18 +0000

    autocommit

--- lectures/lecture12_01/main.cpp
+++ lectures/lecture12_01/main.cpp
@@ -11,53 +11,50 @@ void openinputfile(ifstream& inputfile, string thefilename);
 string getnexthashtag(string texti, int& pos);
 
 
-int main()
-{
+int main() {
     string currentline;
     ifstream inputfile;
     string tag;
-    string t = "#TDSBreakingNews Hillary may delay presidential campaign. Makes the election that much more intense. #TantricCampaigning";
+    string t;
     cout << t << endl;
-
-
- unsigned int hashstart, hashend;
-    cout << "--------------------" << endl;
-
-     hashstart = t.find("#");
-     while ( hashstart < t.length() ) {
-        // Fann hashtag!
-         hashend = t.find_first_of(" .,\n\"#;?!", hashstart+1);
-
-        cout << "hashstart: " << hashstart << endl;
-        cout << "hashend:   " << hashend << endl;
-
-         tag = t.substr(hashstart, hashend - hashstart);
-        cout << "\"" << tag << "\"" << endl;
-
-         hashstart = t.find("#", hashstart+1);
-     }
-
-    /*
     openinputfile(inputfile, THEFILENAME);
 
+
+    unsigned int hashstart, hashend;
+    cout << "--------------------" << endl;
+
+
     while (!inputfile.eof()) {
-        getline(inputfile, currentline);
-        cout << currentline << endl;
+        getline(inputfile, t);
+
+        hashstart = t.find("#");
+        while ( hashstart < t.length() ) {
+            // Fann hashtag!
+            hashend = t.find_first_of(" .,\n\"#;?!", hashstart+1);
+
+            cout << "hashstart: " << hashstart << endl;
+            cout << "hashend:   " << hashend << endl;
+
+            tag = t.substr(hashstart, hashend - hashstart);
+            cout << "\"" << tag << "\"" << endl;
+
+            hashstart = t.find("#", hashstart+1);
+        }
     }
 
-
     inputfile.close();
-*/
 
     return 0;
 }
 
 
 
-void openinputfile(ifstream& inputfile, string thefilename) {
+void openinputfile(ifstream& inputfile, string thefilename)
+{
     inputfile.open(thefilename.c_str());
 
-    if (inputfile.fail()) {
+    if (inputfile.fail())
+    {
         cout << "Can't open " << thefilename << endl;
         exit(1);
     }
