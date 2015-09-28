e81b06eab903b0c5bdcf1d5485123e47f8d1827e
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-28 14:18:53 +0000

    autocommit

--- lectures/lecture13_01/main.cpp
+++ lectures/lecture13_01/main.cpp
@@ -4,18 +4,21 @@ using namespace std;
 
 int main()
 {
-    int a[10];
-    int *p;
+    int n;
+    cin >> n;
 
-    for (int i = 0; i < 10; i++) {
-        a[i] = i;
+    int *p;
+    p = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        p[i] = i;
     }
 
-    p = a;
-
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < n; i++) {
         cout << p[i] << endl;
     }
+
+    delete [] p;
 
     return 0;
 }
