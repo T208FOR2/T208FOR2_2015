434644756edae219a1d03fb7b059b26b67245d90
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-28 13:40:22 +0000

    autocommit

--- lectures/lecture13_01/main.cpp
+++ lectures/lecture13_01/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
     int tala1 = 42, tala2 = 23;
-    int* p1, p2;
+    int *p1, *p2;
 
-    cout << tala1 << " - " << tala2 << endl;
+    cout << &tala1 << " - " << &tala2 << endl;
 
     return 0;
 }
