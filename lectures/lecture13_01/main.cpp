1f897bee5ff802d0c9e143636433fd919a1ede59
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-09-28 13:42:24 +0000

    autocommit

--- lectures/lecture13_01/main.cpp
+++ lectures/lecture13_01/main.cpp
@@ -7,7 +7,11 @@ int main()
     int tala1 = 42, tala2 = 23;
     int *p1, *p2;
 
-    cout << &tala1 << " - " << &tala2 << endl;
+    p1 = &tala1;
+    p2 = &tala2;
+
+    cout << tala1 << " - " << tala2 << endl;
+    cout << *p1 << " - " << *p2 << endl;
 
     return 0;
 }
