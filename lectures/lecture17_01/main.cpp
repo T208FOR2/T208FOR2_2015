26977afeb2edc243bbf0a04bfeafe3fcadaab56b
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-10-12 13:22:05 +0000

    autocommit

--- lectures/lecture17_01/main.cpp
+++ lectures/lecture17_01/main.cpp
@@ -4,13 +4,19 @@ using namespace std;
 
 class Timi {
 public:
-
+    int minutur;
+    int klst;
 };
 
 
 int main()
 {
+    Timi t1;
 
+    t1.klst = 13;
+    t1.minutur = 21;
+
+    cout << t1.klst << ":" << t1.minutur << endl;
 
     return 0;
 }
