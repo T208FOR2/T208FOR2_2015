f6d1ee6dbe4ebaf25a3b32a341514c1670170903
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-10-12 13:36:18 +0000

    autocommit

--- lectures/lecture17_01/main.cpp
+++ lectures/lecture17_01/main.cpp
@@ -6,7 +6,8 @@ class Timi {
 public:
     void set_klst(int k);
     void set_minutur(int m);
-
+    int get_klst();
+    int get_minutur();
 private:
     int minutur;
     int klst;
@@ -20,6 +21,13 @@ void Timi::set_minutur(int m) {
     minutur = m;
 }
 
+int Timi::get_klst() {
+    return klst;
+}
+int Timi::get_minutur() {
+    return minutur;
+}
+
 
 int main()
 {
@@ -28,12 +36,12 @@ int main()
     t1.set_klst(13);
     t1.set_minutur(21);
 
-    cout << t1.klst << ":" << t1.minutur << endl;
+    cout << t1.get_klst() << ":" << t1.get_minutur() << endl;
 
     t1.set_klst(908374);
     t1.set_minutur(-448934);
 
-    cout << t1.klst << ":" << t1.minutur << endl;
+    cout << t1.get_klst() << ":" << t1.get_minutur() << endl;
 
     return 0;
 }
