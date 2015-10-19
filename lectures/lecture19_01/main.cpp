e8c0b2cced91fc7481f032617d37411083323de1
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-10-19 13:21:48 +0000

    autocommit

--- lectures/lecture19_01/main.cpp
+++ lectures/lecture19_01/main.cpp
@@ -3,14 +3,23 @@
 using namespace std;
 
 class Numbers {
+public:
+    Numbers();
 
 private:
     int n;  //Fjšldi staka
     int *p;  //Bendir ‡ fylki af n tšlum
 };
 
+Numbers::Numbers() {
+    cout << "Constructor!" << endl;
+    n = 0;
+    p = NULL;
+}
+
 int main()
 {
+    Numbers thenumbers;
 
     return 0;
 }
