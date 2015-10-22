5ec35e040e4bbaee00ab287207eb22e562f0a27f
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-10-22 09:33:39 +0000

    autocommit

--- lectures/lecture20_01/main.cpp
+++ lectures/lecture20_01/main.cpp
@@ -4,22 +4,23 @@
 using namespace std;
 
 
-void dummyfunction(Numbers &num) {
-  cout << num << endl;
+void dummyfunction(Numbers num) {
+    cout << num << endl;
 }
 
 int main() {
     Numbers thenumbers;
 
-    for (int j = 0; j < 100; j++) {
-      thenumbers.add(j);
+    for (int j = 0; j < 100; j++)
+    {
+        thenumbers.add(j);
     }
 
     dummyfunction(thenumbers);
 
     cout << "Hi!" << endl;
 
-  return 0;
+    return 0;
 }
 
 
