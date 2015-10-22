44448b2fd11aa085f80a33360224d0d5001fc15f
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-10-22 09:31:37 +0000

    autocommit

--- lectures/lecture20_01/main.cpp
+++ lectures/lecture20_01/main.cpp
@@ -9,23 +9,15 @@ void dummyfunction(Numbers &num) {
 }
 
 int main() {
-  for (int i = 0; i < 5; i++) {
-    cout << "----------------" << endl;
     Numbers thenumbers;
 
     for (int j = 0; j < 100; j++) {
       thenumbers.add(j);
     }
 
-    cout << thenumbers - 1 << endl;
-
-    thenumbers[0] = 99;
-
-    //cout << thenumbers << endl;
     dummyfunction(thenumbers);
 
     cout << "Hi!" << endl;
-  }
 
   return 0;
 }
