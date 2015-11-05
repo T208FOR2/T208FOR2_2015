7566e9310f0d39298668519dd72423200c0431e8
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-11-05 08:43:47 +0000

    autocommit

--- lectures/lecture24_01/main.cpp
+++ lectures/lecture24_01/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+void printstring(string s) {
+    cout << s << endl;
+}
+
 int main()
 {
-
+    printstring("Hellothere!");
     return 0;
 }
